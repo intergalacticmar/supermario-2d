8b71b85355b7d04af5b858cb952b7ed3a75769c2
María <120553496+intergalacticmar@users.noreply.github.com>
2023-01-18 11:00:05 +0100

    camaraysalto

--- Assets/scripts/playercontroler.cs
+++ Assets/scripts/playercontroler.cs
@@ -6,11 +6,20 @@ public class playercontroler : MonoBehaviour
 {
     int playerHealth = 3;
     public float playerSpeed = 5.5f;
+    public float jumpForce = 3f;
     string texto = "Hello World";
-    bool isGrounded = false;
+    public SpriteRenderer spriteRender;
+    private Rigidbody2D rBody;
+    private GroundSensor sensor;
+    float horizontal;
+
     // Start is called before the first frame update
     void Start()
     {
+        spriteRender = GetComponent<SpriteRenderer>();
+        rBody = GetComponent<Rigidbody2D>();
+        sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>();
+
         playerHealth = 10;
         Debug.Log(texto);
     }
@@ -18,6 +27,21 @@ public class playercontroler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3 (Input.GetAxis("Horizontal"), 0, 0) * playerSpeed * Time.deltaTime;       
+        horizontal = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontal, 0, 0) * playerSpeed * Time.deltaTime; 
+
+        if(horizontal < 0) 
+        {
+            spriteRender.flipX = true;
+        }  
+        else if(horizontal > 0) 
+        {
+            spriteRender.flipX = false;
+        }
+          
+          if(Input.GetButtonDown("Jump") && sensor.isGrounded)
+          {
+            rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+          }
     }
 }
